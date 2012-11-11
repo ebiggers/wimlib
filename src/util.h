85cc44d618877b8dbedf56fd8f5454a948a636a3
Eric Biggers <ebiggers3@gmail.com>
2012-11-11 13:04:22 -0600

    Add rbtree files

--- src/util.h
+++ src/util.h
@@ -52,6 +52,19 @@ typedef unsigned uint;
 #define swap(a, b) ({typeof(a) _a = a; (a) = (b); (b) = _a;})
 #endif
 
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ *
+ */
+#ifndef container_of
+#define container_of(ptr, type, member) ({			\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+#endif
+
 #define ARRAY_LEN(array) (sizeof(array) / sizeof((array)[0]))
 
 #define ZERO_ARRAY(array) memset(array, 0, sizeof(array))
