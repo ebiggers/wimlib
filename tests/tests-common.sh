08a55221fc6904ceaf4bd1c0532eb03e792d5420
Eric Biggers <ebiggers3@gmail.com>
2012-11-22 00:03:01 -0600

    Test suite without coreutils

---
+++ tests/tests-common.sh
@@ -0,0 +1,35 @@
+srcdir="${srcdir:-.}/.."
+srcdir="$(cd $srcdir; pwd)"
+
+if stat -c %i . ; then
+	BSD_STAT=0
+else
+	BSD_STAT=1
+fi
+
+get_inode_number() 
+{
+	if [ "$BSD_STAT" -eq 1 ]; then
+		stat -f %i "$1"
+	else
+		stat -c %i "$1"
+	fi
+}
+
+get_link_count()
+{
+	if [ "$BSD_STAT" -eq 1 ]; then
+		stat -f %l "$1"
+	else
+		stat -c %h "$1"
+	fi
+}
+
+get_file_size()
+{
+	if [ "$BSD_STAT" -eq 1 ]; then
+		stat -f %z "$1"
+	else
+		stat -c %s "$1"
+	fi
+}
