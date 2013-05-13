1f50611288c0549ef7408a4fd6642f55efc4cd99
Eric Biggers <ebiggers3@gmail.com>
2013-05-13 12:56:47 -0500

    unix_apply.c, unix_capture.c:  Add, fix copyright notices

--- src/unix_apply.c
+++ src/unix_apply.c
@@ -1,3 +1,26 @@
+/*
+ * unix_apply.c - Code to apply files from a WIM image on UNIX.
+ */
+
+/*
+ * Copyright (C) 2012, 2013 Eric Biggers
+ *
+ * This file is part of wimlib, a library for working with WIM files.
+ *
+ * wimlib is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * wimlib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wimlib; if not, see http://www.gnu.org/licenses/.
+ */
+
 #include "config.h"
 
 #ifdef HAVE_UTIME_H
