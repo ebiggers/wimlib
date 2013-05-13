fcc8d33acee4c9ab11ebe975da030c6e53b182e3
Eric Biggers <ebiggers3@gmail.com>
2013-05-13 12:54:44 -0500

    win32_*.c:  Add copyright notices

--- src/win32_apply.c
+++ src/win32_apply.c
@@ -1,3 +1,26 @@
+/*
+ * win32_apply.c - Windows-specific code for applying files from a WIM image.
+ */
+
+/*
+ * Copyright (C) 2013 Eric Biggers
+ *
+ * This file is part of wimlib, a library for working with WIM files.
+ *
+ * wimlib is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * wimlib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wimlib; if not, see http://www.gnu.org/licenses/.
+ */
+
 #ifdef __WIN32__
 
 #include <aclapi.h> /* for SetSecurityInfo() */
--- src/win32_common.c
+++ src/win32_common.c
@@ -1,3 +1,28 @@
+/*
+ * win32_common.c - Windows code common to applying and capturing images, as
+ * well as replacements for various functions not available on Windows, such as
+ * fsync().
+ */
+
+/*
+ * Copyright (C) 2013 Eric Biggers
+ *
+ * This file is part of wimlib, a library for working with WIM files.
+ *
+ * wimlib is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * wimlib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wimlib; if not, see http://www.gnu.org/licenses/.
+ */
+
 #ifdef __WIN32__
 
 #include <shlwapi.h> /* for PathMatchSpecW() */
