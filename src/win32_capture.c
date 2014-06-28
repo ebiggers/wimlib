ebiggers/wimlib

commit e9aed5bf5c3296f35ff7007cb1702c07b7dc2b37

win32_capture.c: Don't add duplicate backslashes

src/win32_capture.c
@@ -406,11 +406,18 @@ winnt_recurse_directory(HANDLE h,
 							   info->FileName[1] == L'.'))
 			{
 				wchar_t *p;
+				wchar_t *filename;
 				struct wim_dentry *child;
 
 				p = full_path + full_path_nchars;
-				*p++ = L'\\';
-				p = wmempcpy(p, info->FileName,
+				/* Only add a backslash if we don't already have
+				 * one.  This prevents a duplicate backslash
+				 * from being added when the path to the capture
+				 * dir had a trailing backslash.  */
+				if (*(p - 1) != L'\\')
+					*p++ = L'\\';
+				filename = p;
+				p = wmempcpy(filename, info->FileName,
 					     info->FileNameLength / 2);
 				*p = '\0';
 
@@ -419,7 +426,7 @@ winnt_recurse_directory(HANDLE h,
 							h,
 							full_path,
 							p - full_path,
-							full_path + full_path_nchars + 1,
+							filename,
 							info->FileNameLength / 2,
 							params,
 							stats,

commit 9d38cdb02481552e1f3c7f56181623b2b60fe857

win32_capture.c:  Fix exclusion when capture path ends in slash
Broken by e9aed5bf5c3296f35ff7007cb1702c07b7dc2b37

src/win32_capture.c
@@ -1396,6 +1396,7 @@ win32_build_dentry_tree(struct wim_dentry **root_ret,
 	int ret;
 	UNICODE_STRING ntpath;
 	struct winnt_scan_stats stats;
+	size_t ntpath_nchars;
 
 	/* WARNING: There is no check for overflow later when this buffer is
 	 * being used!  But it's as long as the maximum path length understood
@@ -1417,16 +1418,20 @@ win32_build_dentry_tree(struct wim_dentry **root_ret,
 		goto out_free_path;
 	}
 
-	params->capture_root_nchars = ntpath.Length / sizeof(wchar_t);
-	wmemcpy(path, ntpath.Buffer, params->capture_root_nchars);
-	path[params->capture_root_nchars] = L'\0';
+	ntpath_nchars = ntpath.Length / sizeof(wchar_t);
+	wmemcpy(path, ntpath.Buffer, ntpath_nchars);
+	path[ntpath_nchars] = L'\0';
+
+	params->capture_root_nchars = ntpath_nchars;
+	if (path[ntpath_nchars - 1] == L'\\')
+		params->capture_root_nchars--;
 
 	HeapFree(GetProcessHeap(), 0, ntpath.Buffer);
 
 	memset(&stats, 0, sizeof(stats));
 
 	ret = winnt_build_dentry_tree_recursive(root_ret, NULL,
-						path, params->capture_root_nchars,
+						path, ntpath_nchars,
 						L"", 0, params, &stats, 0);
 out_free_path:
 	FREE(path);