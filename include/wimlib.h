cfa7869f8c1c7f69d9042ca2d3bc53edb31c16dd
Eric Biggers <ebiggers3@gmail.com>
2016-08-19 20:13:27 -0700

    v1.10.0

--- include/wimlib.h
+++ include/wimlib.h
@@ -11,7 +11,7 @@
 /**
  * @mainpage
  *
- * This is the documentation for the library interface of wimlib 1.9.2, a C
+ * This is the documentation for the library interface of wimlib 1.10.0, a C
  * library for creating, modifying, extracting, and mounting files in the
  * Windows Imaging Format.  This documentation is intended for developers only.
  * If you have installed wimlib and want to know how to use the @b wimlib-imagex
@@ -399,10 +399,10 @@
 #define WIMLIB_MAJOR_VERSION 1
 
 /** Minor version of the library (for example, the 2 in 1.2.5). */
-#define WIMLIB_MINOR_VERSION 9
+#define WIMLIB_MINOR_VERSION 10
 
 /** Patch version of the library (for example, the 5 in 1.2.5). */
-#define WIMLIB_PATCH_VERSION 2
+#define WIMLIB_PATCH_VERSION 0
 
 #ifdef __cplusplus
 extern "C" {
